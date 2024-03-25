93bd77af6f3810e662be4d429c766ab3727b44f7
Zaid khan <160598429+Zaidkhan009@users.noreply.github.com>
2024-03-26 02:08:09 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+function makeShirt(size, text) {
+    console.log("\n You order a ".concat(size, " shirt that says ").concat(Text));
+}
+makeShirt('large', '"i love typescript"');
+makeShirt('medium', '"i need a shirt"');
---
+++ app.ts
@@ -0,0 +1,6 @@
+// function makeShirt(size: String, text: string): void {
+//     console.log(`\n You order a ${size} shirt that says ${Text}`)  
+// }
+
+// makeShirt('large' ,'"i love typescript"')
+// makeShirt('medium' ,'"i need a shirt"')
